d85b43860b82e020f92e82b5dac6caf4fd175d49
bonusinmarch <bonusinmarch@gmail.com>
2016-07-20 16:37:32 +0700

    login splash home

---
+++ app/src/main/java/com/bumpaw/bonuses/shopaholic/AppPreference.java
@@ -0,0 +1,36 @@
+package com.bumpaw.bonuses.shopaholic;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by user on 20/07/2016.
+ */
+public class AppPreference {
+
+    private Context context;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private String PREFS_NAME = "ShopaholicAppPrefs";
+    private String KEYS_USERNAME = "USERNAME";
+
+    public AppPreference(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(PREFS_NAME, context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void setUsername(String username){
+        editor.putString(KEYS_USERNAME, username);
+        editor.commit();
+    }
+
+    public String getUsername(){
+        return sharedPreferences.getString(KEYS_USERNAME,"");
+    }
+
+    public void clear(){
+        editor.clear();
+        editor.commit();
+    }
+}
--- app/src/main/java/com/bumpaw/bonuses/shopaholic/LoginActivity.java
+++ app/src/main/java/com/bumpaw/bonuses/shopaholic/LoginActivity.java
@@ -3,16 +3,21 @@ package com.bumpaw.bonuses.shopaholic;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity implements
         View.OnClickListener{
 
     private TextView tv_reg;
     private Button btn_login;
+    private AppPreference appPreference;
+    private EditText edt_username, edt_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,11 @@ public class LoginActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().setTitle("Login");
+
+        appPreference = new AppPreference(LoginActivity.this);
+
+        edt_password = (EditText)findViewById(R.id.ed_pass);
+        edt_username = (EditText)findViewById(R.id.ed_username);
 
         tv_reg = (TextView)findViewById(R.id.tv_register);
         tv_reg.setOnClickListener(this);
@@ -32,17 +42,30 @@ public class LoginActivity extends AppCompatActivity implements
     @Override
     public void onClick(View v) {
         Intent intent =null;
+        boolean isLogin = false;
         switch (v.getId()){
             case R.id.tv_register:
                 intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 break;
             case R.id.btn_login:
-                intent = new Intent(LoginActivity.this, MainActivity.class);
+                String username = edt_username.getText().toString().trim();
+                String password = edt_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+                }else {
+                    appPreference.setUsername(username);
+                    intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    isLogin = true;
+                }
                 break;
         }
 
         if(intent != null) {
             startActivity(intent);
+            if (isLogin){
+                finish();
+            }
         }
 
 
